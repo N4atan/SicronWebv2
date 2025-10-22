fad779f8963ce938253bf5bf8d7c00a74d06ef8c
ARIEL LORENZ <04529620050@senacrs.edu.br>
2025-10-22 19:45:05 -0300

    alteração do formulario de contato

--- src/components/Forms/Contato/Contato.tsx
+++ src/components/Forms/Contato/Contato.tsx
@@ -9,38 +9,41 @@ export default function ContatoForm() {
     return (
         <form className="contact-form">
 
+            <div className="input-row">
+
+                <Input
+                    variant='default'
+                    label='Qual é o seu nome?'
+                    type='text'
+                    placeholder='Ex.: João da Silva'
+                />
+
+                <Input
+                    variant='default'
+                    label='Seu e-mail:'
+                    type='text'
+                    placeholder='Ex.: joao.silva@email.com'
+                />
+            </div>
+
             <Input
-            variant='default'
-            label='Qual é o seu nome?'
-            type='text'
-            placeholder='Ex.: João da Silva'
+                variant='default'
+                label='Qual o assunto?'
+                type='text'
+                placeholder='Ex.: Dúvida sobre doação'
             />
 
             <Input
-            variant='default'
-            label='Seu e-mail:'
-            type='text'
-            placeholder='Ex.: joao.silva@email.com'
-            />
-
-            <Input
-            variant='default'
-            label='Qual o assunto?'
-            type='text'
-            placeholder='Ex.: Dúvida sobre doação'
-            />
-
-            <Input
-            variant='text-area'
-            label='Sua mensagem:'
-            type='text'
-            placeholder='Descreva sua mensagem aqui...'
+                variant='text-area'
+                label='Sua mensagem:'
+                type='text'
+                placeholder='Descreva sua mensagem aqui...'
             />
 
             <Button
-            variant='primary'
-            type='submit'
-            text='Enviar Mensagem'
+                variant='primary'
+                type='submit'
+                text='Enviar Mensagem'
             />
 
         </form>
