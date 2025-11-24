9dc37ca0544ba93e42af347e5db29085bc0fe9f1
N4atan <gabrielnathan2912@gmail.com>
2025-11-24 19:32:16 -0300

    Fix: Modal Implementado

--- src/components/Forms/EntityUpdate/EntityUpdate.tsx
+++ src/components/Forms/EntityUpdate/EntityUpdate.tsx
@@ -7,6 +7,7 @@ import { Overlay } from "../../Overlay/Overlay";
 import './EntityUpdate.css' // Certifique-se que o arquivo existe
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import Modal from "../../Modal/Modal";
 
 type Props = {
     entity: any;
@@ -25,9 +26,8 @@ export default function EntityUpdate(props: Props) {
         }))
     }
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        // ... sua lógica de submit ...
+    const handleSave = async () => {
+        
         console.log('Dados para salvar:', formData);
         const response = await api.fetchUpdateUser(formData);
 
@@ -44,56 +44,32 @@ export default function EntityUpdate(props: Props) {
     const filteredKeys = keys.filter(k => k != 'id');
 
     return (
-        <Overlay>
-            <Card
-                titleSection={`Editando Entidade`}
-                subtitleSection={`${props.typeEntity} de id: ${props.entity ? props.entity.id : '-'}`}
-                // MUDANÇA AQUI: Layout flexível e altura máxima
-                style={{ 
-                    width: '500px', 
-                    maxHeight: '90vh', 
-                    height: 'auto', 
-                    margin: '25px auto',
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}
+        <Modal
+            title={`Editando ${props.typeEntity}`}
+            pText="Salvar"
+            sText="Cancelar"
+            pEvent={() => handleSave}
+            sEvent={props.onClose}
+            xEvent={props.onClose}
+        >
+            <form
+                className="form-editEntity"
             >
-                <form
-                    className="form-editEntity"
-                    onSubmit={(e) => handleSubmit(e)}
-                >
-                    {/* MUDANÇA AQUI: Envelopamos os inputs nesta div para o scroll funcionar */}
-                    <div className="container-body">
-                        {filteredKeys.map((key, idKey) => (
-                            <Input
-                                key={idKey}
-                                variant="default"
-                                label={key}
-                                value={(formData as any)[key]}
-                                onChange={(e) => handleChange(key, e.target.value)}
-                            />
-                        ))}
-                    </div>
-
-                    <div className="container-buttons">
-                        <Button
-                            variant="secondary"
-                            text="Cancelar"
-                            type="button"
-                            onClick={() => props.onClose()}
-                            style={{ width: '80px' }}
+                
+                <div className="container-body">
+                    {filteredKeys.map((key, idKey) => (
+                        <Input
+                            key={idKey}
+                            variant="default"
+                            label={key}
+                            value={(formData as any)[key]}
+                            onChange={(e) => handleChange(key, e.target.value)}
                         />
+                    ))}
+                </div>
 
-                        <Button
-                            variant="primary"
-                            text="Salvar"
-                            type="submit"
-                            style={{ width: '120px' }}
-                        />
-                    </div>
-                </form>
-            </Card>
-
-        </Overlay>
+                
+            </form>
+        </Modal>
     )
 }
