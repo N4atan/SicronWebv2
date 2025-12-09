51dd8c7792ac8758042376fbedb9d104c9c1eba3
N4atan <gabrielnathan2912@gmail.com>
2025-12-09 13:03:56 -0300

    ADD: Action Buttons in Hero Section

--- src/components/Sections/Hero/HeroSection.tsx
+++ src/components/Sections/Hero/HeroSection.tsx
@@ -1,16 +1,31 @@
 import "./HeroSection.css";
 import saImage from "../../../assets/images/Kids-happy.jpg";
+import Button from "../../Button/Button";
 
 export default function HeroSection() {
   return (
     <section className="hero-section">
       <div className="hero-text">
-        <h1 className="title-section">
+        <h1 className="hero-title">
           Conectando doadores<br />a quem realmente precisa
         </h1>
-        <p className="subtitle-section">
-          Faça a diferença na vida de pessoas de<br />forma segura e transparente
+        
+        <p className="hero-subtitle">
+          Faça a diferença na vida de pessoas de forma segura e transparente. O SICRON garante que sua ajuda chegue ao destino certo, transformando doações digitais em impacto real.
         </p>
+
+        <div className="container-buttons-hero">
+          <Button 
+            variant={'primary'} text={"Conhecer Causas"}  style={{height: '100%'}}
+          />
+
+          <Button 
+            variant={'secondary'}
+            text={'Como funciona ?'}
+            style={{height: '100%'}}
+            onClick={() => document.getElementById('how-section')?.scrollIntoView({ behavior: 'smooth'})}
+          />
+        </div>
       </div>
 
       <div className="hero-image">
