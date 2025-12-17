f31769b2b96140ee9c9de50b64a26e664a43e41b
N4atan <gabrielnathan2912@gmail.com>
2025-12-17 09:05:10 -0300

    Limpeza de Pastas Duplicadas

--- components/Overlay/Overlay.tsx
+++
@@ -1,8 +0,0 @@
-export const Overlay = ({ children }: { children: React.ReactNode }) => (
-    <div
-        style={{width: '100dvw', height: '100dvh', zIndex: '2000', position: 'fixed', top: 0, left: 0, backgroundColor: 'rgba(0, 0, 0, 0.3)'}}
-    >
-        {children}
-        
-    </div>
-)
